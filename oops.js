6bca30b30a1f174894ef389c9cf338a355ae8085
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-05-03 23:10:25 +0530

    JSON

---
+++ oops.js
@@ -0,0 +1,28 @@
+// Object Oriented Programming in Javascript
+// allows to create objects without defining the class
+
+// One of way creating Javascript (JSON) -> Javascript Object Notation.
+
+/*
+var bird = {
+
+    x : 100,
+    y : 50,
+    color : "Blue",
+    eggs : ["One", "Two", "Three", "Four"],
+
+    fly: function(){
+        console.log("Bird is flying at this ", this.x, this.y, " coordinates");
+    }
+};
+
+// for loop
+for (let i = 0; i < bird.eggs.length; i++) {
+    console.log(bird.eggs[i]);
+}
+
+// for each loop
+bird.eggs.forEach(function(value, index){
+    console.log(index, value);
+});
+*/
