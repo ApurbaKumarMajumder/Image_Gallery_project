6b3b1c2a38089190999ac019ed87083a2615f170
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-05-03 22:21:46 +0530

    more practice

--- practice.js
+++ practice.js
@@ -12,14 +12,45 @@ c = 20; // This is a Global Variable
 var d = 30; // Function Scope
 let e = 50; // Block Scope
 
+// function fun(){
+
+//     let a = 5;
+//     if (a === 5) {
+//         let b = 10; // let has a block scope only, like a if statement or a while statement
+//         console.log( "Inside the If statement ", b);
+//     }
+//     console.log("Inside the func ", b);
+// }
+
+// fun();
+
+// function fun(){
+
+//     let a = 5;
+//     if (a === 5) {
+//         var b = 10; // var has a function scope, i.e it can be used all around the function.
+//         console.log( "Inside the If statement ", b);
+//     }
+//     console.log("Inside the func ", b);
+// }
+
+// fun();
+
 function fun(){
 
     let a = 5;
     if (a === 5) {
-        let b = 10;
+        b = 100; // this is a global scope variable, available along the whole code.
         console.log( "Inside the If statement ", b);
     }
     console.log("Inside the func ", b);
 }
 
 fun();
+console.log("Outside the function ", b);
+
+function square_root(n) {
+    return  Math.sqrt(n);
+}
+
+console.log(square_root(10));
