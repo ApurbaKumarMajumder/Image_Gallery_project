b224011e53c8ff6ff7ca345c82e2a6bbc46df8b6
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-05-03 23:39:29 +0530

    oops intro

--- oops.js
+++ oops.js
@@ -26,3 +26,58 @@ bird.eggs.forEach(function(value, index){
     console.log(index, value);
 });
 */
+
+// Create objects
+
+// One way
+var apple = {
+    taste : "Sweet",
+    color : "Red",
+}
+
+// another way
+function Fruit(taste, color){
+    this.color = color;
+    this.taste = taste;
+}
+
+// new keyword
+let Mango = new Fruit("Sweet", "Yellow");
+let orange = new Fruit("Sour", "orange");
+
+// console.log(Mango)
+// console.log(orange)
+// console.log(apple)
+
+
+// Class keyword for creating objects (ECMAScript 2015)
+
+// Class Declaration (Not Hoisted)
+class FruitClass{
+    constructor(taste, color){
+        this.color = color;
+        this.taste = taste;
+    }
+};
+
+// Functions - Declaration, Function Expression
+
+let kiwi = new FruitClass("sour", "green");
+
+// Class Expression (Hoisted)
+
+let FruitClass2 = class {
+    constructor(taste, color){
+        this.color = color;
+        this.taste = taste;
+    }
+};
+
+
+// Functions - Declaration, Function Expression
+
+let kiwi2 = new FruitClass2("sour", "green");
+
+console.log(apple);
+console.log(kiwi);
+console.log(kiwi2);
