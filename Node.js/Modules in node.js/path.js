4df814db39478580f35d5547c3609b7f3e04c88a
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-05-16 14:10:01 +0530

    path file added into modules in node.js

---
+++ Node.js/Modules in node.js/path.js
@@ -0,0 +1,26 @@
+// Folder -> Directory
+// House -> Address to access the house
+// File/Folder -> Address -> path
+// Webpage -> Address -> URL
+
+const path = require("path");
+
+//"toberemoved" -> (filename only is required to access) when you are in the same folder
+// "C:\\Users\\9851a\\Documents" -> absolute path
+// relative path -> ../../Module0/lecture3.2/boxSizing
+
+// path module
+console.log("Directory Name: ", __dirname); // current folder in which we have our js file absolute path
+console.log("File Name: ", __filename); // current file ka path
+// abhi ye script app me convert hone ke baad ki path pe chal rahi hai
+console.log(process.cwd()); // cwd -> current working directory
+
+let path = require("path");
+
+let folderName = "C:";
+let nextFolderName = "Movies";
+let childFolder = "bollywood";
+let actualFile = "Rang_De_Basanti.mp4";
+let address = path.join(folderName, nextFolderName, childFolder, actualFile);
+console.log("``````````````````````````````````````");
+console.log(address);
