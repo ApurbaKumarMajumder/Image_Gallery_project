874ab8eeb027830c9fe6ee41410da0131ab36391
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-05-22 14:42:07 +0530

    more on path.js

--- Node.js/Modules in node.js/path.js
+++ Node.js/Modules in node.js/path.js
@@ -13,14 +13,21 @@ const path = require("path");
 console.log("Directory Name: ", __dirname); // current folder in which we have our js file absolute path
 console.log("File Name: ", __filename); // current file ka path
 // abhi ye script app me convert hone ke baad ki path pe chal rahi hai
-console.log(process.cwd()); // cwd -> current working directory
+console.log("current working directory: ", process.cwd()); // cwd -> current working directory
 
-let path = require("path");
+// let path = require("path");
 
 let folderName = "C:";
 let nextFolderName = "Movies";
 let childFolder = "bollywood";
 let actualFile = "Rang_De_Basanti.mp4";
+// path join
 let address = path.join(folderName, nextFolderName, childFolder, actualFile);
 console.log("``````````````````````````````````````");
-console.log(address);
+console.log("address ", address);
+
+// .js
+let extname = path.extname(address);
+console.log("extname ", extname);
+let basename = path.basename(address);
+console.log("basename ", basename);
