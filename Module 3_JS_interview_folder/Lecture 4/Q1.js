0b231ce659f3fcb819ab52436ade1a7d65944450
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-06-03 03:09:56 +0530

    more on lecture 4 in interview folder

---
+++ Module 3_JS_interview_folder/Lecture 4/Q1.js
@@ -0,0 +1,16 @@
+function printArrFN() {
+    let arr = [];
+    for (var i = 0; i < 3; i++) {
+        function printI() {
+            console.log('i', i);
+        }
+        // adding reference to the array
+        arr.push(printI);
+    }
+    return arr;
+}
+
+let arrFn = printArrFN();
+arrFn[0]();
+arrFn[1]();
+arrFn[2]();
